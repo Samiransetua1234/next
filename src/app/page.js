73a13bcb12f49579e5605706af98de349056f83f
Samiran1234 <samiransetua26@gmail.com>
2024-10-28 12:25:22 +0530

    function event

--- src/app/page.js
+++ src/app/page.js
@@ -1,21 +1,19 @@
+'use client'
 import styles from "./page.module.css";
 
 export default function Home() {
+  const fruit = (item) =>{
+    alert(item)
+  }
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <h1>Home Page!!!</h1>
-        <User name='Samiran'/>
+        <h1>Events , Function and State</h1>
+        <button onClick={()=> fruit('apple')}>Click Me</button>
       </main>
      
     </div>
   );
 }
 
-const User = ({name}) =>{
-  return (
-    <div>
-      <h2>User is {name}</h2>
-    </div>
-  )
-}
+
